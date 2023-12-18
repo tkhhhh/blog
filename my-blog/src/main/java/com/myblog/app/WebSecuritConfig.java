57fe0a652791ef7fbd31218af8edfacbb9a2b669
tonysken <626627983@qq.com>
2023-12-18 20:39:27 +0000

    update security setting

--- my-blog/src/main/java/com/myblog/app/WebSecuritConfig.java
+++ my-blog/src/main/java/com/myblog/app/WebSecuritConfig.java
@@ -4,19 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class WebSecuritConfig {
     @Bean
-    public HttpSecurity httpSecurity(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.
             authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home").permitAll()
                                     .anyRequest().authenticated())
-            .formLogin((form) -> form.loginPage("/lohin").permitAll())
+            .formLogin((form) -> form.loginPage("/login").permitAll())
             .logout((logout) -> logout.permitAll());
-        return http;
+        return http.build();
     }
-
-    
 }
